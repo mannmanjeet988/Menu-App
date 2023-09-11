50fa57ed90f874551986119fdc876f1a0a566eff
mannmanjeet988 <mannmanjeet988@gmail.com>
2023-09-11 14:00:15 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,14 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+{/* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> */}
 
+import React from "react"
+import MenuComp from "./MenuComp";
+
+const App = () =>{
+    return(
+        <div>
+            <MenuComp />
+        </div>
+    )
+}
+
+export default App;
